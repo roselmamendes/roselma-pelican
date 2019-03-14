2d403d8f57c5f7fdc7bf952351d8c7340859c534
Roselma Mendes <roselma.mendes@gmail.com>
2019-03-14 14:54:35 -0300

    Adiciona css para os posts.

--- cli/blog.sh
+++ cli/blog.sh
@@ -6,6 +6,11 @@ echo "Building the blog"
 docker run --rm -v "$PWD":/usr/src/app -it roselma-pelican pelican
 ;;
 
+'build-layout-watch')
+echo "Building the blog"
+docker run --rm -v "$PWD":/usr/src/app -it roselma-pelican pelican -r
+;;
+
 'start-server')
 echo "Starting server"
 docker run --rm -v "$PWD/docs":/usr/src/app -p 8000:8000 -it roselma-pelican python -m http.server --bind 0.0.0.0
